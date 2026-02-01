969bd7256f4d866e0d6b1ca1b01450cbe3d7775b
Michael Lapcevich <149459288+lapcevichme@users.noreply.github.com>
2026-02-01 21:26:55 +0700

    feat: added ktor to dependencies and di

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -62,6 +63,7 @@ dependencies {
     implementation(libs.material.icons)
     implementation(libs.material.icons.extended)
     implementation(libs.kizitonwose.calendar)
+    implementation(libs.bundles.ktor.client)
 }
 
 
