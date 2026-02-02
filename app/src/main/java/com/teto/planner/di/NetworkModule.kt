5dd8bc19f03ac4cc0a3f3012fdabd00cadd57df7
Michael Lapcevich <149459288+lapcevichme@users.noreply.github.com>
2026-02-02 17:13:27 +0700

    feat: added ktor auth

--- app/src/main/java/com/teto/planner/di/NetworkModule.kt
+++ app/src/main/java/com/teto/planner/di/NetworkModule.kt
@@ -8,6 +8,9 @@ import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BasicAuthCredentials
+import io.ktor.client.plugins.auth.providers.basic
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.Logger
@@ -22,6 +25,8 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+    private const val BASE_URL = "http://10.0.2.2:8080/api"
+
     @Provides
     @Singleton
     fun provideHttpClient(): HttpClient {
@@ -35,19 +40,32 @@ object NetworkModule {
             }
 
             defaultRequest {
-                url("") // TODO
+                url(BASE_URL)
                 header(HttpHeaders.ContentType, ContentType.Application.Json)
             }
 
-            install(HttpTimeout) {
-                requestTimeoutMillis = 10_000
+            install(Auth) {
+                basic {
+                    credentials {
+                        BasicAuthCredentials(username = "ivan", password = "password")
+                    }
+                    sendWithoutRequest { true }
+                }
             }
+
+            install(HttpTimeout) {
+                requestTimeoutMillis = 15_000
+                connectTimeoutMillis = 15_000
+                socketTimeoutMillis = 15_000
+            }
+
             install(Logging) {
                 logger = object : Logger {
                     override fun log(message: String) {
-                        Log.d("Ktor", message)
+                        Log.d("Ktor-Network", message)
                     }
                 }
+                level = io.ktor.client.plugins.logging.LogLevel.ALL
             }
         }
     }
